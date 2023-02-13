7241f4dfec0040476f6d697512173dcff143e350
Szabolcs Kurdi <szabolcs.kurdi@gmail.com>
2023-02-13 17:28:22 +0100

    refactor

--- scripts/utils/string/slugify.js
+++ scripts/utils/string/slugify.js
@@ -3,10 +3,12 @@ const removeAccents = require('../i18n/removeAccents');
 /**
  * Removes special characters from a string,
  * trying to leave us with an id-like construct.
+ * It also cuts away `()` and domain-like suffixes.
  */
 module.exports = function slugify(text = '') {
   return removeAccents(String(text || ''))
     .toLocaleLowerCase()
+    .replace(/\.(hu|com|sk)/, '')
     .replace(/[\s.\-_:,&'"]/g, '')
     .replaceAll('-', '')
     .replaceAll('–', '')
